e7a14bff7bba95ccdd3c3d2bf102b0703fa84468
benjaminr177 <unoceptik@gmail.com>
2018-01-03 23:58:10 +0200

    brain.js setup

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const brain = require('brain.js')
-const data = require('sh')
+const data = require('./dataset').data
+const getAccuracy = require('./getAccuracy').getAccuracy
+
+const SPLIT = 99
+const trainingData = data.slice(0, SPLIT)
+const testData = data.slice(SPLIT + 1)
+
+const net = brain.NeuralNetwork({
+  activation: 'sigmoid',
+  hiddenLayers: [5],
+  iterations: 500,
+  learningRate: 0.5,
+})
+
+net.train(trainingData)
+
+const accuracy = getAccuracy(net, testData)
+console.log('accuracy: ' + accuracy)
